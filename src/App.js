d8c4731126f3e22e6776692c315d548cacaeb667
1eedaegon <d8726243@gmail.com>
2021-03-16 20:15:50 +0900

    feat: todo app using useReducer
    useReducer 연습을 위한 간단한 todo앱 예제

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 
-const INCREMENT = "increment";
-const DECREMENT = "decrement";
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case INCREMENT:
-      return { count: state.count + 1 };
-    case DECREMENT:
-      return { count: state.count - 1 };
+const ADD = "add";
+const initState = {
+  todos: []
+}
+const todoReducer = (state, actions) => {
+  switch (actions.type) {
+    case ADD:
+      return { todos: [...state.todos, { text: actions.paylaod }] }
     default:
-      throw new Error();
+      return new Error();
   }
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 })
-
+  const [state, dispatch] = useReducer(todoReducer, initState)
+  const [newTodo, setNewTodo] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+    dispatch({ type: ADD, paylaod: newTodo })
+  }
+  const onChange = (event) => {
+    const { target: { value } } = event;
+    setNewTodo(value)
+  }
   return (
     <div className="App">
-      <h1>{state.count}</h1>
-      <button onClick={() => dispatch({ type: INCREMENT })}>ADD</button>
-      <button onClick={() => dispatch({ type: DECREMENT })}>SUB</button>
+      <h1>Add a Todo</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={newTodo}
+          type='text'
+          placeholder="Write to todo"
+          onChange={onChange}
+        />
+      </form>
+      <h2>Todos</h2>
+      <ul>
+        {state.todos.map((todo, idx) => {
+          return <li key={idx}>{todo.text}</li>
+        })}
+      </ul>
     </div>
   );
 }
